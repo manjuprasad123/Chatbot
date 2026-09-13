c39a5dc81dbbc325f92aee66b2b57324f5fe4bdf
Manjuprasad M <manjuprasadm04@gmail.com>
2026-09-13 13:41:30 +0530

    Improve sidebar UI and hover tooltips

--- Chatbot/src/components/Sidebar.jsx
+++ Chatbot/src/components/Sidebar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react'
+import menuIcon from '../assets/Screenshot 2026-09-13 125154.png';
 import './Sidebar.css'
 
 export function Menubar(){
@@ -14,11 +15,58 @@ export function Menubar(){
         <div className="js-chat-header-content">
           <button className="js-chat-title-btn"
           onClick={ToogleSideBar} >
-            M
+              <img 
+                src={menuIcon}
+                alt="Chatbot"
+                className="sidebar-title-icon"/>
           </button>
           <h2 className="js-chat-title">Chatbot</h2>
         </div>
+
+        <div className="js-chat-header-content">
+          <button className="js-chat-btn">
+            <img 
+              src="https://img.icons8.com/fluency-systems-regular/48/pen-squared.png" 
+              alt="NewChat"
+              className="sidebar-icon"
+            />
+          </button>
+          <span className="js-chat">NewChat</span>
+        </div>
+
+        <div className="js-chat-header-content">
+          <button className="js-chat-btn">
+            <img 
+              src="https://img.icons8.com/fluency-systems-regular/48/stack-of-photos--v1.png" 
+              alt="Images"
+              className="sidebar-icon"
+            />
+          </button>
+          <span className="js-chat">Images</span>
+        </div>
+
+        <div className="js-chat-header-content">
+          <button className="js-chat-btn">
+            <img 
+              src="https://img.icons8.com/fluency-systems-filled/48/search.png" 
+              alt="Search"
+              className="sidebar-icon"
+            />
+          </button>
+          <span className="js-chat">Search</span>
+        </div>
         
+        <div className="js-chat-header-content">
+          <button className="js-chat-btn">
+            <img 
+              src="https://img.icons8.com/fluency-systems-regular/48/speech-bubble--v1.png" 
+              alt="Recent"
+              className="sidebar-icon"
+            />
+          </button>
+          <span className="js-chat">Recent</span>
+        </div>
+
       </div>
   );
 }
